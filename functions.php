c2e810a22abdae405f9d95e982de1bc679ada308
Jan <janhajk@gmail.com>
2021-04-27 19:19:08 +0000

    undo remove google & facebook

--- functions.php
+++ functions.php
@@ -41,4 +41,45 @@ function wpglorify_woocommerce_password_filter() {
 }
 
 
+
+/***** Adding Facebook Pixel *****/
+// add_action('wp_head', 'oiw_add_fbpixel');
+
+function oiw_add_fbpixel() {
+?>
+<!-- Facebook Pixel Code -->
+      <script>
+      !function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)}(window, document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+      fbq('init', '2735434730048399');
+      fbq('track', 'PageView');
+      </script>
+      <noscript><img height="1" width="1" style="display:none"
+      src="https://www.facebook.com/tr?id=2735434730048399&ev=PageView&noscript=1"
+      /></noscript>
+<!-- End Facebook Pixel Code -->
+<?php
+}
+
+
+add_action('wp_head', 'mask_add_google_analytics', 20);
+function mask_add_google_analytics() {
+      ?>
+      <!-- Global site tag (gtag.js) - Google Analytics -->
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-7KTWM28FPP"></script>
+      <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-7KTWM28FPP');
+      </script>
+      <?php
+}
+
 ?>
